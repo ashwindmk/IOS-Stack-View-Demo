33c2017dd4b1f393d82cd9f440725e73f4ef33ad
Ashwin Dinesh <ashwin.dinesh01@gmail.com>
2017-10-20 16:10:33 +0530

    Initial commit

---
+++ IOS-Stack-View-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS-Stack-View-Demo
+//
+//  Created by Upendra Karki on 20/10/17.
+//  Copyright © 2017 Ashwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
